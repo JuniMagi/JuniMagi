4e4f093014f83e47ba940ebc956c61e76c3d286b
JuniMagi <140687588+JuniMagi@users.noreply.github.com>
2023-08-03 18:30:36 +0800

    Add files via upload

---
+++ rockpaperscissorst.py
@@ -0,0 +1,73 @@
+import random
+def bot():
+    a = (int(random.randrange(1,4)))
+    if a == 1:
+        print ("Computer Picked Rock")
+        return 1
+    elif a == 2:
+        print ("Computer Picked Paper")
+        return 2
+    elif a == 3:
+        print ("Computer Picked Scissors")
+        return 3
+    else :
+        print("cpfail")
+def playerin():
+    a = input("Rock, Paper or Scissors? ")
+    if a == "Rock":
+        print("You picked Rock")
+        return 1
+    elif a == "Paper" :
+        print("You picked Paper")
+        return 2
+    elif a == "Scissors":
+        print("You picked Scissors")
+        return 3
+    else :
+        print("invalid")
+        main()
+
+def main():
+    y = playerin()
+    x = bot()
+    if x == y:
+        print("It's a tie!")
+    elif (x == 1 and y == 2) or (x == 2 and y == 3 ) or (x == 3 and y == 1):
+        print("Playerwin")
+        return "xwin"
+    else:
+        print('Computerwin')
+        return "ywin"
+
+def wincon():
+    gameover=False
+    Pscore = 0
+    Bscore = 0
+    while not gameover:
+        s = main()
+        if s == "xwin":
+            Pscore += 1
+            print("again")
+            print("Player Score: " , Pscore)
+            print("Bot Score: ", Bscore)
+        elif s =="ywin":
+            Bscore += 1
+            print("again")
+            print("Player Score: " , Pscore)
+            print("Bot Score: ", Bscore)
+        else:
+            Pscore += 1
+            Bscore += 1
+            print("again")
+            print("Player Score: " , Pscore)
+            print("Bot Score: ", Bscore)
+        if Bscore == 5 :
+            print("Bot win")
+            gameover = True
+        elif Pscore == 5 :
+            print("Player win")
+            gameover = True
+
+
+wincon()
+input()
